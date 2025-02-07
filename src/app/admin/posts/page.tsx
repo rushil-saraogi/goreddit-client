fe3240d068fc9eabfbb0497c38e35d44be792f58
Nilomee Jesrani <home@NilomeeMacPersonal.local>
2025-02-07 09:35:21 -0500

    ES lint errors

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -68,7 +68,7 @@ export default () => {
             });
     }
 
-    const handlePostClick = (id: number) => {
+    const handlePostClick = (id: string) => {
         router.push(`/admin/posts/${id}`);
     }
 
